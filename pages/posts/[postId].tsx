86c5fa078233a8f086432b68e20614276113f096
Thang Vu <thangvu.dona@gmail.com>
2023-12-02 03:09:42 +0700

    02-07: SSG getStaticPaths
    - getStaticPaths: specific all paths
    - getStaticProps: render all HTML files (in build mode)

--- pages/posts/[postId].tsx
+++ pages/posts/[postId].tsx
@@ -1,16 +1,58 @@
+import { GetStaticPaths, GetStaticProps, GetStaticPropsContext } from "next";
 import { useRouter } from "next/dist/client/router";
 import React from "react";
 
-export interface PostDetailPageProps {}
+export interface PostPageProps {
+  post: any;
+}
 
-export default function PostDetailPage(props: PostDetailPageProps) {
+export default function PostDetailPage({ post }: PostPageProps) {
   const router = useRouter();
+
+  if (!post) return;
 
   return (
     <div>
       <h1>Post Detail Page</h1>
 
-      <p>Query: {JSON.stringify(router.query)}</p>
+      <p>{post.author}</p>
+      <p>{post.title}</p>
+      <p>{post.description}</p>
     </div>
   );
 }
+
+export const getStaticPaths: GetStaticPaths = async () => {
+  console.log("\nGET STATIC PATHS");
+
+  const response = await fetch(
+    "https://js-post-api.herokuapp.com/api/posts?_page=1 "
+  );
+  const data = await response.json();
+
+  return {
+    paths: data.data.map((post: any) => ({ params: { postId: post.id } })),
+    fallback: false,
+  };
+};
+
+export const getStaticProps: GetStaticProps<PostPageProps> = async (
+  context: GetStaticPropsContext
+) => {
+  // server-side
+  // build time
+  console.log("\nGET STATIC PROPS", context.params?.postId);
+  const postId = context.params?.postId;
+  if (!postId) return { notFound: true };
+
+  const response = await fetch(
+    `https://js-post-api.herokuapp.com/api/posts/${postId}`
+  );
+  const data = await response.json();
+
+  return {
+    props: {
+      post: data,
+    },
+  };
+};
--- pages/posts/index.tsx
+++ pages/posts/index.tsx
@@ -1,4 +1,5 @@
 import { GetStaticProps, GetStaticPropsContext } from "next";
+import Link from "next/link";
 import React from "react";
 
 export interface PostListPageProps {
@@ -12,7 +13,11 @@ export default function PostListPage({ posts }: PostListPageProps) {
 
       <ul>
         {posts.map((post) => (
-          <li key={post.id}>{post.title}</li>
+          <li key={post.id}>
+            <Link href={`/posts/${post.id}`}>
+              <a>{post.title}</a>
+            </Link>
+          </li>
         ))}
       </ul>
     </div>
@@ -24,13 +29,11 @@ export const getStaticProps: GetStaticProps<PostListPageProps> = async (
 ) => {
   // server-side
   // build time
-  console.log("static props, only on server");
 
   const response = await fetch(
     "https://js-post-api.herokuapp.com/api/posts?_page=1 "
   );
   const data = await response.json();
-  console.log("data: ", data);
 
   return {
     props: {
