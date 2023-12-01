db59f1973605fa7e1a0b8231efcd55e52ecc6cec
Thang Vu <thangvu.dona@gmail.com>
2023-12-02 01:43:12 +0700

    02-06: SSG getStaticProps with typescript
    - Static HTML Generation (N/A)
    - Static HTML + JSON Data (getStaticProps)
    - <script id="__NEXT_DATA__" type="application/json">

--- pages/posts/index.tsx
+++ pages/posts/index.tsx
@@ -1,7 +1,40 @@
+import { GetStaticProps, GetStaticPropsContext } from "next";
 import React from "react";
 
-export interface PostListPageProps {}
-
-export default function PostListPage(props: PostListPageProps) {
-  return <div>Post List Page</div>;
+export interface PostListPageProps {
+  posts: any[];
 }
+
+export default function PostListPage({ posts }: PostListPageProps) {
+  return (
+    <div>
+      <h2>Post List Page</h2>
+
+      <ul>
+        {posts.map((post) => (
+          <li key={post.id}>{post.title}</li>
+        ))}
+      </ul>
+    </div>
+  );
+}
+
+export const getStaticProps: GetStaticProps<PostListPageProps> = async (
+  context: GetStaticPropsContext
+) => {
+  // server-side
+  // build time
+  console.log("static props, only on server");
+
+  const response = await fetch(
+    "https://js-post-api.herokuapp.com/api/posts?_page=1 "
+  );
+  const data = await response.json();
+  console.log("data: ", data);
+
+  return {
+    props: {
+      posts: data.data.map((x: any) => ({ id: x.id, title: x.title })),
+    },
+  };
+};
