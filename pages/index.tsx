b9a6b34786c9cb19949e6ebd5814acbd03dfbe3d
Thang Vu <thangvu.dona@gmail.com>
2023-12-01 21:52:27 +0700

    02-03: Navigate between pages
    - <Link><a>...</a></Link> from 'next/link'.
    - useRouter, router.push,..
    - refetching page in the background, appear on the viewport.

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,23 @@
-import type { NextPage } from 'next'
-import Head from 'next/head'
-import Image from 'next/image'
-import styles from '../styles/Home.module.css'
+import type { NextPage } from "next";
+import Head from "next/head";
+import Image from "next/image";
+import Link from "next/link";
+import styles from "../styles/Home.module.css";
+import { useRouter } from "next/dist/client/router";
 
 const Home: NextPage = () => {
+  const router = useRouter();
+
+  const gotoDetailPage = () => {
+    router.push({
+      pathname: "posts/[postId]",
+      query: {
+        postId: "12345",
+        ref: "cab",
+      },
+    });
+  };
+
   return (
     <div className={styles.container}>
       <Head>
@@ -18,9 +32,15 @@ const Home: NextPage = () => {
         </h1>
 
         <p className={styles.description}>
-          Get started by editing{' '}
+          Get started by editing{" "}
           <code className={styles.code}>pages/index.js</code>
         </p>
+
+        <Link href="/about">
+          <a>Go to About</a>
+        </Link>
+
+        <button onClick={gotoDetailPage}>Go to Detail Page</button>
 
         <div className={styles.grid}>
           <a href="https://nextjs.org/docs" className={styles.card}>
@@ -59,14 +79,14 @@ const Home: NextPage = () => {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Powered by{' '}
+          Powered by{" "}
           <span className={styles.logo}>
             <Image src="/vercel.svg" alt="Vercel Logo" width={72} height={16} />
           </span>
         </a>
       </footer>
     </div>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
