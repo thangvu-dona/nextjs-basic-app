8fc297fed72d2035f99615f67ed1fdc080e3c5a9
Thang Vu <thangvu.dona@gmail.com>
2023-12-02 10:33:02 +0700

    02-08: SSG and Data Fetching on client side
    - dynamic ssr=false(dont render on server side)
    - SSG + data fetching on client side(useEffect + shallow redering)

--- pages/about.tsx
+++ pages/about.tsx
@@ -1,7 +1,75 @@
-import React from "react";
+import Header from "@/components/common/header";
+import { useRouter } from "next/router";
+import React, { useEffect, useState } from "react";
+// import dynamic from "next/dynamic";
+
+// const Header = dynamic(() => import("@/components/common/header"), {
+//   ssr: false, // prevent render Header on Server side, only render on Client side
+// });
 
 export interface AboutPageProps {}
 
 export default function AboutPage(props: AboutPageProps) {
-  return <div>About Page</div>;
+  const [postList, setPostList] = useState([]);
+  const router = useRouter();
+
+  console.log("About query: ", router.query);
+  const page = router.query?.page;
+
+  // use Effect only run on client side, not run on server side
+  useEffect(() => {
+    if (!page) return;
+
+    (async () => {
+      const response = await fetch(
+        `https://js-post-api.herokuapp.com/api/posts?_page=${page}`
+      );
+      const data = await response.json();
+
+      setPostList(data.data);
+    })();
+  }, [page]);
+
+  function handleNextClick() {
+    router.push(
+      {
+        pathname: "/about",
+        query: {
+          page: (Number(page) || 1) + 1,
+        },
+      },
+      undefined,
+      { shallow: true }
+    );
+  }
+
+  return (
+    <div>
+      <h2>About Page</h2>
+      <Header />
+      <ul className="post-list">
+        {postList.map((post: any) => (
+          <li key={post.id}>{post.title}</li>
+        ))}
+      </ul>
+
+      <button onClick={handleNextClick}>Next page</button>
+    </div>
+  );
 }
+
+// SSG
+export async function getStaticProps() {
+  console.log("get static props");
+
+  return {
+    props: {},
+  };
+}
+
+// turn on server side rendering
+// export async function getServerSideProps() {
+//   return {
+//     props: {}, // will be passed to the page component as props
+//   };
+// }
